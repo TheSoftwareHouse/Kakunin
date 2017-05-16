5470258ad715629023ff79205e62e4f3cb52add5
Adam Polak <adampolak@Adams-Mac-mini.local>
2017-05-16 19:44:51 +0200

    Refactor of date comparator and first tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
   "extends": "airbnb/legacy",
+  "parserOptions": {
+    "ecmaVersion": 2017
+  },
   "rules": {
-    "no-plusplus": 0
+    "no-plusplus": 0,
+    "no-trailing-spaces": ["error", {
+      "skipBlankLines": true
+    }]
   },
 
   "env": {
     "browser": true,
     "node": true,
-    "protractor": true
+    "protractor": true,
+    "mocha": true
   }
 };
