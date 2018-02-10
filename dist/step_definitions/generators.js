d8dda6dad6d5a2e62085e158cd64cac4db311c81
Tomasz Górski <tomasz.gorski@thesoftwarehouse.pl>
2018-02-11 00:07:20 +0100

    Removed params generator step. Improved generate step - use transformer instead of generators

--- dist/step_definitions/generators.js
+++ dist/step_definitions/generators.js
@@ -6,16 +6,12 @@ var _variableStore = require('../helpers/variable-store.helper');
 
 var _variableStore2 = _interopRequireDefault(_variableStore);
 
-var _generators = require('../generators');
+var _transformers = require('../transformers');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 (0, _cucumber.defineSupportCode)(function ({ When }) {
-  When(/^I generate random "([^"]*)" as "([^"]*)"$/, function (generatorName, variableName) {
-    return _generators.generators.generate(generatorName).then(result => _variableStore2.default.storeVariable(variableName, result));
-  });
-
-  When(/^I generate random "([^"]*)" "([^"]*)" as "([^"]*)"$/, function (generatorName, generatorParam, variableName) {
-    return _generators.generators.generate(generatorName, generatorParam).then(result => _variableStore2.default.storeVariable(variableName, result));
+  When(/^I generate random "([^"]*)" as "([^"]*)"$/, function (transformerName, variableName) {
+    return _transformers.transformers.transform(transformerName).then(result => _variableStore2.default.storeVariable(variableName, result));
   });
 });
--- src/step_definitions/generators.js
+++ src/step_definitions/generators.js
@@ -1,16 +1,9 @@
 import { defineSupportCode } from 'cucumber';
 import variableStore from '../helpers/variable-store.helper';
-import { generators } from '../generators';
+import { transformers } from '../transformers';
 
 defineSupportCode(function ({ When }) {
-  When(/^I generate random "([^"]*)" as "([^"]*)"$/, function (generatorName, variableName) {
-    return generators.generate(generatorName).then(result => variableStore.storeVariable(variableName, result));
-  });
-
-  When(/^I generate random "([^"]*)" "([^"]*)" as "([^"]*)"$/, function (generatorName, generatorParam, variableName) {
-    return generators.generate(generatorName, generatorParam).then(result => variableStore.storeVariable(
-      variableName,
-      result
-    ));
+  When(/^I generate random "([^"]*)" as "([^"]*)"$/, function (transformerName, variableName) {
+    return transformers.transform(transformerName).then(result => variableStore.storeVariable(variableName, result));
   });
 });
