98b5bfc638cecaaaf3b6cad347f22b3fb2e0914e
Tomasz Górski <tomek.m.gorski@gmail.com>
2018-03-14 01:29:41 +0100

    Added unit tests covering isRelativePage method

--- src/helpers/url-parser.helper.spec.js
+++ src/helpers/url-parser.helper.spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { waitForUrlChangeTo } from '../helpers/url-parser.helper';
+import { isRelativePage, waitForUrlChangeTo } from '../helpers/url-parser.helper';
 
 const exampleBaseUrl = 'https://example-base-url.com';
 
@@ -106,45 +106,71 @@ describe('URL parser', () => {
       .to.equal(false);
   });
 
-  it('returns object with properties defined in a page URL - two wild cards', () => {
-    expect(waitForUrlChangeTo('https://google.com/:example/:name', 'https://google.com/example/janek')
-      .bind(null, exampleBaseUrl)())
-      .to.eql({ example: 'example', name: 'janek' });
-  });
-
-  it('returns object with properties defined in a page URL - one wild card', () => {
+  it('returns object with properties defined in a page URL - one wildcard', () => {
     expect(waitForUrlChangeTo('https://google.com/:name', 'https://google.com/janek')
       .bind(null, exampleBaseUrl)())
       .to.eql({ name: 'janek' });
   });
 
-  it('returns object with properties defined in a page URL - one wild card inside a long URL', () => {
+  it('returns object with properties defined in a page URL - one wildcard inside a long URL', () => {
     expect(waitForUrlChangeTo('https://google.com/account/:username/settings/display', 'https://google.com/account/janek/settings/display')
       .bind(null, exampleBaseUrl)())
       .to.eql({ username: 'janek' });
   });
 
-  it('returns false if a host is incorrect - one wild card in relative path', () => {
+  it('returns false if a host is incorrect - one wildcard in relative path', () => {
     expect(waitForUrlChangeTo('/account/settings/:userType', 'https://incorrect-host/account/settings/admin')
       .bind(null, 'https://google.com')())
       .to.equal(false);
   });
 
-  it('returns false if a URL is incorrect - one wild card in relative path', () => {
+  it('returns false if a URL is incorrect - one wildcard in relative path', () => {
     expect(waitForUrlChangeTo('/account/settings/:userType/something', 'https://incorrect-host/account/settings/admin')
       .bind(null, 'https://google.com')())
       .to.equal(false);
   });
 
-  it('returns false if a URL is incorrect - one wild card in absolute path', () => {
+  it('returns false if a URL is incorrect - one wildcard in absolute path', () => {
     expect(waitForUrlChangeTo('https://incorrect-host/account/settings/:userType/something', 'https://incorrect-host/account/settings/admin')
       .bind(null, exampleBaseUrl)())
       .to.equal(false);
   });
 
-  it('returns object with properties defined in a page URL - one wild card in relative path', () => {
+  it('returns object with properties defined in a page URL - one wildcard in relative path', () => {
     expect(waitForUrlChangeTo('/account/settings/:userType', 'https://google.com/account/settings/user')
       .bind(null, 'https://google.com')())
       .to.eql({ userType: 'user' });
+  });
+
+  it('returns false if the URL is absolute - http without path', () => {
+    expect(isRelativePage('http://google.com')).to.equal(false);
+  });
+
+  it('returns false if the URL is absolute - https without path', () => {
+    expect(isRelativePage('https://google.com')).to.equal(false);
+  });
+
+  it('returns false if the URL is absolute - http with path', () => {
+    expect(isRelativePage('http://google.com/with-path')).to.equal(false);
+  });
+
+  it('returns false if the URL is absolute - https with parameter', () => {
+    expect(isRelativePage('https://google.com/:param/with-path')).to.equal(false);
+  });
+
+  it('returns true if the URL is relative', () => {
+    expect(isRelativePage('/with-path')).to.equal(true);
+  });
+
+  it('returns true if the URL is relative - with parameter', () => {
+    expect(isRelativePage('/:path-with-parameter')).to.equal(true);
+  });
+
+  it('returns true if the URL contains only slash', () => {
+    expect(isRelativePage('/')).to.equal(true);
+  });
+
+  it('returns true if the URL is empty', () => {
+    expect(isRelativePage('')).to.equal(true);
   });
 });
