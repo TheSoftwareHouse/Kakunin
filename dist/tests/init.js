f176884eecdfc5240c5c29ee9a92633982f480a8
Adam Nowrot <adam.nowrot@thesoftwarehouse.pl>
2019-04-08 08:01:54 +0200

    removed unnecessary files

--- dist/tests/init.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const protractor = require("protractor");
-// set global by to allow recursive call on src directory
-global.by = new protractor.ProtractorBy();
-require('../core/prototypes');
