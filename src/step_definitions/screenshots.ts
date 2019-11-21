8175c7f3fe5ee160d2c4493c107ee23c72bfb72f
Jozef Szymala <jozef.szymala@thesoftwarehouse.pl>
2019-11-21 12:26:02 +0100

    Change existSync to fs.access, removed process.cwd

--- src/step_definitions/screenshots.ts
+++ src/step_definitions/screenshots.ts
@@ -1,21 +1,24 @@
 import { When, Then } from 'cucumber';
 import config from '../core/config.helper';
-import { existsSync } from 'fs';
-import { resolve } from 'path';
+import { access } from 'fs';
+import * as path from 'path';
 
-const diffExists = screenshotName => {
-  if (
-    existsSync(
-      resolve(
-        process.cwd(),
-        `${config.temporaryFolder}/diff`,
+const diffExists = async screenshotName => {
+  return new Promise((resolve, reject) => {
+    return access(
+      path.resolve(
+        `${config.imageComparator.temporaryFolder}/diff`,
         `${screenshotName}-${config.browserWidth}x${config.browserHeight}.png`
-      )
-    )
-  ) {
-    return Promise.reject(`File "${screenshotName}" in the baseline is different! Check "diff" catalog.`);
-  }
-  return Promise.resolve;
+      ),
+      status => {
+        status
+          ? resolve()
+          : reject(
+              `Check "diff" catalog. Devation tolerance was bigger then: ${config.imageComparator.saveAboveTolerance}%`
+            );
+      }
+    );
+  });
 };
 
 When(/^I take screenshot of the element "([^"]*)" and save as a "([^"]*)"$/, async function(
@@ -37,19 +40,15 @@ Then(/^I compare the screenshot of the element "([^"]*)" saved as "([^"]*)"$/, a
   elementName,
   screenshotName
 ) {
-  await browser.imageComparison.checkElement(this.currentPage.getElement(elementName), screenshotName).then(() => {
-    diffExists(screenshotName);
-  });
+  await browser.imageComparison
+    .checkElement(this.currentPage.getElement(elementName), screenshotName)
+    .then(() => diffExists(screenshotName));
 });
 
 Then(/^I compare the screenshot of visible the part of the page saved as "([^"]*)"$/, async screenshotName => {
-  await browser.imageComparison.checkScreen(screenshotName).then(() => {
-    diffExists(screenshotName);
-  });
+  await browser.imageComparison.checkScreen(screenshotName).then(() => diffExists(screenshotName));
 });
 
 Then(/^I compare the full screenshot of the page  saved as "([^"]*)"$/, async screenshotName => {
-  await browser.imageComparison.checkFullPageScreen(screenshotName).then(() => {
-    diffExists(screenshotName);
-  });
+  await browser.imageComparison.checkFullPageScreen(screenshotName).then(() => diffExists(screenshotName));
 });
