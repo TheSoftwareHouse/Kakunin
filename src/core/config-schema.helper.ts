c02b964d6df5d6215d83e500a3723adea8c7317d
Jozef Szymala <jozef.szymala@thesoftwarehouse.pl>
2019-11-27 09:51:14 +0100

    Changes for the PR, removed unnecessary required(), removed min max values etc

--- src/core/config-schema.helper.ts
+++ src/core/config-schema.helper.ts
@@ -2,115 +2,64 @@ import * as Joi from '@hapi/joi';
 import chalk from 'chalk';
 
 const schema = Joi.object({
-  browserWidth: Joi.number()
-    .min(100)
-    .max(2000)
+  baseUrl: Joi.string()
+    .pattern(/\b((http|https):\/\/?)[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/?))/)
     .required(),
-  browserHeight: Joi.number()
-    .min(100)
-    .max(2000)
+  type: Joi.string()
+    .valid('ng1', 'ng2', 'otherWeb')
     .required(),
-  browserLanguage: Joi.string().required(),
+  browserWidth: Joi.number(),
+  browserHeight: Joi.number(),
+  browserLanguage: Joi.string(),
   timeout: Joi.number()
     .min(1)
     .required(),
-  elementsVisibilityTimeout: Joi.number()
-    .min(1)
-    .required(),
-  waitForPageTimeout: Joi.number()
-    .min(1)
-    .required(),
-  downloadTimeout: Joi.number()
-    .min(1)
-    .required(),
+  intervalEmail: Joi.number(),
+  maxEmailRepeats: Joi.number(),
+  elementsVisibilityTimeout: Joi.number().min(1),
+  waitForPageTimeout: Joi.number().min(1),
+  downloadTimeout: Joi.number().min(1),
+  emails: Joi.array().items(Joi.string()),
   reports: Joi.string().required(),
-  downloads: Joi.string().required(),
-  data: Joi.string().required(),
+  downloads: Joi.string(),
+  data: Joi.string(),
   features: Joi.array()
     .items(Joi.string())
     .required(),
   pages: Joi.array()
     .items(Joi.string())
     .required(),
-  matchers: Joi.array()
-    .items(Joi.string())
-    .required(),
-  generators: Joi.array()
-    .items(Joi.string())
-    .required(),
-  form_handlers: Joi.array()
-    .items(Joi.string())
-    .required(),
+  matchers: Joi.array().items(Joi.string()),
+  generators: Joi.array().items(Joi.string()),
+  form_handlers: Joi.array().items(Joi.string()),
   step_definitions: Joi.array()
     .items(Joi.string())
     .required(),
-  comparators: Joi.array()
-    .items(Joi.string())
-    .required(),
-  dictionaries: Joi.array()
-    .items(Joi.string())
-    .required(),
-  transformers: Joi.array()
-    .items(Joi.string())
-    .required(),
-  regexes: Joi.array()
-    .items(Joi.string())
-    .required(),
-  hooks: Joi.array()
-    .items(Joi.string())
-    .required(),
+  comparators: Joi.array().items(Joi.string()),
+  dictionaries: Joi.array().items(Joi.string()),
+  transformers: Joi.array().items(Joi.string()),
+  regexes: Joi.array().items(Joi.string()),
+  hooks: Joi.array().items(Joi.string()),
   imageComparator: {
     baselineFolder: Joi.string().required(),
-    temporaryFolder: Joi.string().required(),
+    temporaryFolder: Joi.string(),
     saveAboveTolerance: Joi.number()
-      .min(1)
-      .max(99)
-      .required(),
+      .min(0)
+      .max(99),
   },
-  clearEmailInboxBeforeTests: Joi.boolean().required(),
-  clearCookiesAfterScenario: Joi.boolean().required(),
-  clearLocalStorageAfterScenario: Joi.boolean().required(),
-  email: Joi.string()
-    .email({ minDomainSegments: 2 })
-    .allow(null),
-  headless: Joi.boolean().required(),
-  noGpu: Joi.boolean().required(),
-  type: Joi.string()
-    .valid('ng1', 'ng2', 'otherWeb')
-    .required(),
-  baseUrl: Joi.string()
-    .pattern(/\b((http|https):\/\/?)[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/?))/)
-    .required(),
-  apiUrl: Joi.string()
-    .pattern(/\b((http|https):\/\/?)[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/?))/)
-    .required(),
-  browserMob: {
-    serverPort: Joi.number().required(),
-    port: Joi.number().required(),
-    host: Joi.string().required(),
-  },
-  browserstack: {
-    seleniumAddress: Joi.string().pattern(/\b((http|https):\/\/?)[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/?))/),
-    defaultPort: Joi.number(),
-    capabilities: {
-      'browserstack.user': Joi.string()
-        .alphanum()
-        .empty(''),
-      'browserstack.key': Joi.string()
-        .alphanum()
-        .empty(''),
-      'browserstack.local': Joi.boolean().required(),
-      browserName: Joi.string()
-        .valid('firefox', 'chrome', 'IE', 'safari', 'opera')
-        .required(),
-    },
-  },
+  clearEmailInboxBeforeTests: Joi.boolean(),
+  clearCookiesAfterScenario: Joi.boolean(),
+  clearLocalStorageAfterScenario: Joi.boolean(),
+  email: Joi.any(),
+  headless: Joi.boolean(),
+  noGpu: Joi.boolean(),
+  browserMob: Joi.any(),
   accounts: {
     someAccount: {
       accounts: Joi.array().items(Joi.object()),
     },
   },
-  performance: Joi.boolean().required(),
+  performance: Joi.boolean(),
 });
 
 export const validateConfig = config => {
