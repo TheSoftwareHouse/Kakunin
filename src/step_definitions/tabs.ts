93ecf95262c45f78107cafb814c41cf7c4b8e971
Stanisz Szymon <stanisz.szymon@gmail.com>
2019-11-28 10:48:41 +0100

    tabs to navigation methods

--- src/step_definitions/tabs.ts
+++ src/step_definitions/tabs.ts
@@ -1,12 +1,10 @@
 import { When } from 'cucumber';
+import { methods } from '../web/methods';
 
 When(/^I switch to window number "([^"]*)" of a browser$/, tabNumber => {
-  return browser.getAllWindowHandles().then(handles => browser.switchTo().window(handles[tabNumber - 1]));
+  return methods.navigation.switchWindow(tabNumber);
 });
 
 When(/^I close the current browser tab$/, () => {
-  return browser
-    .close()
-    .then(() => browser.getAllWindowHandles())
-    .then(tabs => browser.switchTo().window(tabs[0]));
+  return methods.navigation.closeCurrentWindow();
 });
--- src/web/methods/index.ts
+++ src/web/methods/index.ts
@@ -1,5 +1,7 @@
 import * as interactions from './interactions.methods';
+import * as navigation from './navigation.methods';
 
 export const methods = {
   interactions,
+  navigation,
 };
